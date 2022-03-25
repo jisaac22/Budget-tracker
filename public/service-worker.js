8c55fbc8ef6118e9d042f074800c5c9a7dce8e37
jacob sierra <jacobisaac22@yahoo.com>
2022-03-24 20:28:24 -0500

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,6 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-
-// install
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -45,7 +43,6 @@ self.addEventListener("fetch", function(evt) {
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -53,7 +50,6 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
@@ -61,8 +57,6 @@ self.addEventListener("fetch", function(evt) {
 
     return;
   }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
 
   evt.respondWith(
     caches.match(evt.request).then(function(response) {
